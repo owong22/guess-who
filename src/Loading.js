3e6c9c5428594677bbebde8067664cfe82167b73
owong22 <itsoscarwong@gmail.com>
2023-03-01 17:40:54 -0800

    Smaller loading for responsiveness

--- src/Loading.js
+++ src/Loading.js
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="w-[400px] h-[400px] mx-auto my-0 border-2 border-gray-600 rounded-full animate-spin border-t-gray-200"></div>
+    <div className="w-[50px] h-[50px] mx-auto my-0 border-2 border-gray-600 rounded-full animate-spin border-t-gray-200 sm:w-[400px] sm:h-[400px]"></div>
   );
 };
 
